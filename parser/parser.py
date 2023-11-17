60c52b7777c0e6391345e0529d764858756fe1e2
Roman Iashchenko <rsyaschenko_1@edu.hse.ru>
2023-11-17 14:41:18 +0300

    add tag(dont work), timeout and some comments

--- parser/parser.py
+++ parser/parser.py
@@ -1,11 +1,12 @@
 # извините я не разделил на классы аааааа
 import csv
-import requests
+import requests as re
 from bs4 import BeautifulSoup
 
 with open('/Users/macbook/Downloads/hse_x_td_url.csv', 'r', encoding='utf-8') as file:
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/91.0.4472.124 Safari/537.36 '
     }
 
     csv_reader = csv.reader(file, delimiter=';')
@@ -19,14 +20,27 @@ with open('/Users/macbook/Downloads/hse_x_td_url.csv', 'r', encoding='utf-8') as
         print(f"URL: {url}, Категория: {base_category_nm}")
 
         try:
-            response = requests.get(url, headers=headers)
+            response = re.get(url, headers=headers, timeout=10)
 
             if response.status_code == 200:
-                soup = BeautifulSoup(response.text, 'html.parser')
-                all_text = ' '.join([element.get_text(strip=True) for element in soup.find_all(string=True)])
-                print(f"All Text: {all_text}")
+                if response.status_code == 200:
+                    soup = BeautifulSoup(response.text, 'html.parser')
+
+                    # извлекаем description
+                    meta_description = soup.find('meta', attrs={'name': 'description'})
+
+                    # получаем его содержимое, если есть
+                    description_content = meta_description[
+                        'content'] if meta_description and 'content' in meta_description.attrs else ''
+
+                    # получаем весь текст со страницы
+                    all_text = ' '.join([element.get_text(strip=True) for element in soup.find_all(string=True)])
+
+                    all_text_with_description = f"{description_content} {all_text}"
+
+                    print(f"All Text with Description: {all_text_with_description}")
             elif response.status_code != 403 and response.status_code != 404:
                 print(f"Ошибка при получении страницы: {response.status_code}")
         except Exception as e:
-            if not isinstance(e, requests.ConnectionError):
+            if not isinstance(e, re.ConnectionError):
                 print(f"Ошибка при обработке URL {url}: {type(e).__name__}, {e}")
