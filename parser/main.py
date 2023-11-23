c8fb43e8966178d556d6f780f465a100b07931cf
Rodion Chernomordin <rodion.chernomordin@gmail.com>
2023-11-23 13:11:07 +0300

    create working class Parser

---
+++ parser/main.py
@@ -0,0 +1,11 @@
+from parser import Parser
+
+
+def main():
+    pars = Parser()
+    table_path = ""
+    pars.parse_from_table(table_path)
+
+
+if __name__ == '__main__':
+    main()
--- parser/parser.py
+++ parser/parser.py
@@ -1,30 +1,48 @@
 from bs4 import BeautifulSoup
-import requests
+import requests as re
 import csv
 
 
 class Parser:
-    def parse_a(self):
-        pass
+    @staticmethod
+    def return_url_info(url: str) -> str | None:
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/91.0.4472.124 Safari/537.36 '
+        }
 
-    def return_url_info(self, url: str) -> str:
         url = "https://" + url
-        result = ""
 
         # Проверяет, что можно через requests.get(url)
         try:
-            response = requests.get(url)
-        except requests.exceptions.ConnectionError:
-            return ""
+            response = re.get(url, headers=headers, timeout=10)
 
-        # рабочая страница
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, 'html.parser')
 
-        return result
+                # извлекаем description
+                meta_description = soup.find('meta',
+                                             attrs={'name': 'description'})
 
-    def parse(self, table: str):
-        with open(table, 'r') as file:
+                # получаем его содержимое, если есть
+                description_content = meta_description[
+                    'content'] if meta_description and 'content' in meta_description.attrs else ''
+
+                # получаем весь текст со страницы
+                all_text = ' '.join(
+                    [element.get_text(strip=True) for element in
+                     soup.find_all(string=True)])
+
+                all_text_with_description = f"{description_content} {all_text}"
+                return all_text_with_description
+            elif response.status_code != 403 and response.status_code != 404:
+                print(f"Ошибка при получении страницы: {response.status_code}")
+        except Exception as e:
+            if not isinstance(e, re.ConnectionError):
+                print(f"Ошибка при обработке URL {url}: {type(e).__name__}, {e}")
+
+    def parse_from_table(self, table: str) -> None:
+        with open(table, 'r', encoding='utf-8') as file:
             csv_reader = csv.reader(file, delimiter=';')
             next(csv_reader)
 
@@ -33,10 +51,10 @@ class Parser:
                 base_category_nm = row[1]
 
                 print(f"URL: {url}, Категория: {base_category_nm}")
+
                 result = self.return_url_info(str(url))
 
-                if result == "":
+                if result is None:
                     continue
 
-                print(result)
-                break
+                print("All Text with Description:", result)
